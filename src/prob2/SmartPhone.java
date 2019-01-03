e2aede15fbb85f8f8be84c191a720f8afc0da4d4
bitec <bitec@bitec-PC>
2019-01-03 16:12:52 +0900

    문제 해결

--- src/prob2/SmartPhone.java
+++ src/prob2/SmartPhone.java
@@ -1,4 +1,24 @@
 package prob2;
 
-public class SmartPhone extends MusicPhone {
+public class SmartPhone extends MusicPhone 
+{
+	@Override
+	protected void playMusic() {
+		// TODO Auto-generated method stub
+		System.out.println("다운로드해서 음악재생");
+	}
+	
+	@Override
+	public void execute(String function) {
+		// TODO Auto-generated method stub
+		if (function.equals("앱"))
+			appExecute();
+		else
+			super.execute(function);
+	}
+	
+	public void appExecute()
+	{
+		System.out.println("앱실행");
+	}
 }
