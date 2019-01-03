c5c263fa13dfd102710f1a7f15ad7c6e4ba23b48
bitec <bitec@bitec-PC>
2019-01-03 17:21:20 +0900

    문제 해결

--- src/prob5/MyStack.java
+++ src/prob5/MyStack.java
@@ -2,19 +2,47 @@ package prob5;
 
 public class MyStack {
 	private int top;
-	private String[] buffer;
+	public String[] buffer;
 
 	@SuppressWarnings("unchecked")
-	public MyStack(int capacity) {
+	public MyStack(int capacity) 
+	{
+		buffer = new String[capacity];
+		top = 0;
 	}
 
-	public void push(String s) {
+	public void push(String s) 
+	{
+		if (top != buffer.length-1)
+			buffer[top++] = s; 
+		else
+		{
+			String[] doubleSpaceArr = new String[buffer.length * 2];
+			System.arraycopy(buffer, 0, doubleSpaceArr, 0, buffer.length);
+			buffer = doubleSpaceArr;		
+			push(s);
+		}
 	}
 
-	public String pop() throws MyStackException {
+	public String pop() throws MyStackException 
+	{
+		if (top != -1)
+		{
+			if (buffer[top] == null)
+			{
+				top--;
+				return buffer[top--];
+			}
+			
+			else
+				return buffer[top--];
+		}
+		else
+			throw new MyStackException("stack is empty");
 	}
 
-	public boolean isEmpty() {
+	public boolean isEmpty() 
+	{
 		return top == -1;
 	}
 }
